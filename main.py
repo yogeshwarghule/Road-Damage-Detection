63221e27d6d392d1a42470816f5b504583bab679
Yogeshwar Ghule <yghule2001@gmail.com>
2022-12-20 17:24:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # screen size
     screen_width = root.winfo_screenwidth()//2
     screen_height = root.winfo_screenheight()//2
-    root.geometry(str(screen_width) + "x" + str(screen_height))
+    root.geometry(str(screen_width) + "x" + str(screen_height))  
     root.resizable(False, False)
 
     # login page
